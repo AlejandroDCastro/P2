5bd80175f9f753e7dbdf96a1d0f828d596753aa2
Sawn39 <33158086+Sawn39@users.noreply.github.com>
2019-07-23 16:55:35 +0200

    práctica casi terminada

--- Matrícula 2018-2019/Práctica 1/Persona.java
+++ Matrícula 2018-2019/Práctica 1/Persona.java
@@ -1,3 +1,69 @@
 // DNI 20521823 CASTRO VALERO, ALEJANDRO
 import java.util.ArrayList;
 
+public class Persona {
+  
+        private String nombre;
+        private Huerta huerta;
+  
+        
+        public Persona(String n) {
+                nombre = (n != null) ? n : "John Doe";
+                huerta = null;
+        }
+  
+  
+        public boolean planta(Planta p, Huerta h) {
+                boolean devuelve_ = false;
+          
+                if (h != null) {
+                        devuelve_ = huerta.planta(p);
+                }
+                
+                return devuelve_;
+        }
+  
+  
+        public Coordenada paseo() {
+                Coordenada devuelve_ = null;
+          
+                if (huerta != null) {
+                        devuelve_ = huerta.getLocalizacion();
+                } else {
+                        
+                        ArrayList<Huerta> loc_ = Huerta.getLocalizadas();
+                        for (int i=0; i<loc_.size(); i++) {
+                                if (loc_.get(i).getCuidador() == null) {
+                                        huerta = loc_.get(i);
+                                        devuelve_ = huerta.getLocalizacion();
+                                        huerta.setCuidador(this);
+                                        break;
+                                }
+                        }
+                  
+                }
+          
+                return devuelve_;
+        }
+  
+  
+        public ArrayList<Planta> malasHierbas() {
+                
+        }
+  
+  
+        public int abona(int , String ) {
+                
+        }
+  
+  
+        public String getNombre() {
+                return nombre;
+        }
+  
+  
+        public Huerta getHuerta() {
+                return huerta;
+        }
+  
+}
